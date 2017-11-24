00ee8b85de89a096225e0dbdeafbbf8d8b6127e4
Angular CLI <angular-cli@angular.io>
2017-11-24 13:38:23 -0800

    chore: initial commit from @angular/cli

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sra-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'sra';
+}
