8be704a92a8e50fbe37d5175a0c722b8b716e6db
Sandeep Thottapilly <sandeep_b_t@hotmail.com>
2017-12-02 12:10:16 -0800

    Base Code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { SharedComponent } from './shared/shared.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    SharedComponent
   ],
   imports: [
     BrowserModule,
